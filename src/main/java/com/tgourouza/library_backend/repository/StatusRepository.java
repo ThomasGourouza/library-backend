33d622fbb3ecd5286f77d265486181df2de83556
Gourouza Thomas <thomas.gourouza@gmail.com>
2025-07-30 11:27:34 +0200

    refacto

--- src/main/java/com/tgourouza/library_backend/repository/StatusRepository.java
+++ src/main/java/com/tgourouza/library_backend/repository/StatusRepository.java
@@ -1,13 +1,9 @@
 package com.tgourouza.library_backend.repository;
 
-import com.tgourouza.library_backend.constant.Status;
 import com.tgourouza.library_backend.entity.StatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface StatusRepository extends JpaRepository<StatusEntity, Long> {
-    Optional<StatusEntity> findByStatus(Status status);
 }
